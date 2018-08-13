20f36bd0100e5b0b5995a28ba3e2c81198fd0861
Geza Kovacs <geza0kovacs@gmail.com>
2018-08-12 23:59:47 -0700

    make startup process more reliable

--- start.sh
+++ start.sh
@@ -2,8 +2,14 @@
 
 #Stop the background process
 sudo /etc/init.d/bluetooth stop
+sleep 1
+while [ $(ps aux | grep bluetoothd | grep -v grep | wc -l) -ne 0 ]; do
+    echo "killall bluetoothd"
+    sudo killall bluetoothd
+    sleep 1
+done
 # Turn on Bluetooth
-sudo hciconfig hcio up
+#sudo hciconfig hcio up
 # Update  mac address
 ./updateMac.sh
 #Update Name
@@ -19,9 +25,9 @@ if [ $? != 0 ]; then
     tmux split-window -h -t BT-HID
     tmux split-window -v -t BT-HID:os.0
     tmux split-window -v -t BT-HID:os.1
-    tmux send-keys -t BT-HID:os.0 'cd $C_PATH && sudo /usr/sbin/bluetoothd --nodetach --debug -p time ' C-m
-    tmux send-keys -t BT-HID:os.1 'cd $C_PATH/server && sudo python btk_server.py ' C-m
-    tmux send-keys -t BT-HID:os.2 'cd $C_PATH && sudo /usr/bin/bluetoothctl' C-m
+    tmux send-keys -t BT-HID:os.0 'cd $C_PATH && sudo /usr/sbin/bluetoothd --compat --nodetach --debug -p time ' C-m
+    tmux send-keys -t BT-HID:os.1 'cd $C_PATH/server && sleep 1 && sudo python btk_server.py ' C-m
+    tmux send-keys -t BT-HID:os.2 'cd $C_PATH && sleep 1 && sudo /usr/bin/bluetoothctl' C-m
     tmux send-keys -t BT-HID:os.3 'cd $C_PATH/keyboard/ && sleep 5 && sudo python kb_client.py' C-m
     tmux set-option -t BT-HID mouse on
 fi
